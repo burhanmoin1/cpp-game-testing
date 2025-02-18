b63393c9235ba3711bd74a272f75c48481252655
burhanmoin1 <m.burhanmoin1@gmail.com>
2025-02-18 23:30:15 +0500

    testng

--- test.cpp
+++ test.cpp
@@ -1,7 +1,57 @@
+
+
 #include <iostream>
+#include <string>  // Required for strcpy
+
 using namespace std;
 
-int main() {
-    cout << "MinGW is working!" << endl;
-    return 0;
+void say_hello() {
+    cout << "Hello" << endl;
 }
+
+int main() {
+//     // Create a 2D vector (3x3 grid)
+//     vector<vector<int>> matrix = {
+//         {1, 2, 3, 5},  // First row
+//         {4, 5, 6},  // Second row
+//         {7, 8, 9}   // Third row
+//     };
+
+//     // Display the 2D vector (matrix)
+//     cout << "Initial Matrix:\n";
+//     for (size_t i = 0; i < matrix.size(); ++i) {
+//         for (size_t j = 0; j < matrix[i].size(); ++j) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     // Modify an element (change the value at row 1, column 2)
+//     matrix[1][2] = 10;
+
+//     // Display the updated matrix
+//     cout << "\nUpdated Matrix:\n";
+//     for (size_t i = 0; i < matrix.size(); ++i) {
+//         for (size_t j = 0; j < matrix[i].size(); ++j) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+// string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+// //     string key {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+// //     std::string secret_message{};
+// //     cout << "Enter your secret message: ";
+// //     getline(cin, secret_message);
+// //     for (char c : secret_message) {
+// //         size_t position = alphabet.find(c);
+// //         if (position != std::string::npos) {
+// //             char new_char {key.at(position)};
+// //         }
+
+say_hello();
+    
+return 0;
+}
+
+
